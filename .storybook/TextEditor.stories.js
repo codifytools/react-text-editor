36c12d3ac5a39791089ff659042992749becf9e9
Juan Pablo Mejia <juanpmd@hotmail.com>
2021-04-24 00:03:25 -0500

    feat(dev environment): add storybook configuration

---
+++ .storybook/TextEditor.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TextEditor } from '../src/index.js';
+
+export default {
+	component: TextEditor,
+	title: 'Text Editor'
+};
+
+export const Placeholder = () => <TextEditor />;

5ccc3e3d056dc48b79db21113fa89053c3c9488f
Juan Pablo Mejia <juanpmd@hotmail.com>
2021-04-29 13:09:36 -0500

    feat(storybook): update stories folder structure + Editor story

--- .storybook/TextEditor.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { TextEditor } from '../src/index.js';
-
-export default {
-	component: TextEditor,
-	title: 'Text Editor'
-};
-
-export const Placeholder = () => <TextEditor />;
---
+++ src/Editor.stories.js
@@ -0,0 +1,29 @@
+import React, { useState, Fragment } from "react";
+import Editor from "./Editor.js";
+
+export default {
+	component: Editor,
+	title: "React Text Editor"
+};
+
+export const Example = () => {
+	const [values, setValues] = useState({ text: '<p>This is our rich text editor and here we will display different options and functionalities</p><p>There are some elements like bold, italic, underline, strikethrough</p>' });
+	
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setValues({ ...values, [name]: value });
+	}
+	
+	return (
+		<Fragment>
+			<Editor
+				field="text"
+				html={values.text}
+				saveCallback={handleChange}
+				placeholder="Temporal placeholder..."
+				/>
+
+			<button onClick={() => console.log(values.text)}>Submit</button>
+		</Fragment>
+	);
+}
